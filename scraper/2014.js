2722dcf59002ecb340cfbfcb41c519003d512a24
Patrick Fisher <patrick@pwfisher.com>
2020-11-29 14:09:45 -0800

    add 2014 and 2015 index fixtures

---
+++ scraper/2014.js
@@ -0,0 +1,296 @@
+module.exports = [
+  {
+    "date": "2014-12-18",
+    "id": "49",
+    "key": "icouldloveyouforever",
+    "title": "i could love you forever"
+  },
+  {
+    "date": "2014-12-18",
+    "id": "48",
+    "key": "itwasstrangetoseeyouagain",
+    "title": "It was strange to see you again"
+  },
+  {
+    "date": "2014-12-16",
+    "id": "47",
+    "key": "thatechoisgettinglouder",
+    "title": "That Echo is Getting Louder"
+  },
+  {
+  "date": "2014-12-16",
+  "id": "46",
+    "key": "robotsarentscary",
+    "title": "Robots Aren't Scary"
+  },
+  {
+    "date": "2014-12-11",
+    "id": "45",
+    "key": "itseverthingaboutyou",
+    "title": "it's everything about you"
+  },
+  {
+    "date": "2014-12-11",
+    "id": "44",
+    "key": "afearofitnotgoingright",
+    "title": "A fear of it not going right"
+  },
+  {
+    "date": "2014-12-09",
+    "id": "43",
+    "key": "icantfindmycharger",
+    "title": "I can't find my charger"
+  },
+  {
+    "date": "2014-12-09",
+    "id": "42",
+    "key": "ilikeseeingyouinthewinter",
+    "title": "I like seeing you in the winter "
+  },
+  {
+    "date": "2014-12-05",
+    "id": "41",
+    "key": "nooneislost",
+    "title": "No one is Lost"
+  },
+  {
+    "date": "2014-12-05",
+    "id": "40",
+    "key": "floatabovethefeeling",
+    "title": "Float Above the Feeling"
+  },
+  {
+    "date": "2014-12-04",
+    "id": "39",
+    "key": "ifidontdoitthenwhowill",
+    "title": "if I don't do it, then who will?"
+  },
+  {
+    "date": "2014-12-04",
+    "id": "38",
+    "key": "idontknowwhyyourealwayshere",
+    "title": "I don't know why you're always here"
+  },
+  {
+    "date": "2014-12-03",
+    "id": "37",
+    "key": "howcanikeepwarmwhenyouresofaraway",
+    "title": "How can I keep warm when you're so far away?"
+  },
+  {
+    "date": "2014-12-03",
+    "id": "36",
+    "key": "ifyoudontwanttoiwill",
+    "title": "If you don't want to, I will"
+  },
+  {
+    "date": "2014-11-26",
+    "id": "35",
+    "key": "ivebeenfollowingyousinceforever",
+    "title": "I've been following you since forever"
+  },
+  {
+    "date": "2014-11-26",
+    "id": "34",
+    "key": "youcancometoo",
+    "title": "You can come too"
+  },
+  {
+    "date": "2014-11-20",
+    "id": "33",
+    "key": "ididntexpectthesuntobesobrightorthenighttobesodark",
+    "title": "I didn't expect the sun to be so bright or the night to be so dark."
+  },
+  {
+    "date": "2014-11-20",
+    "id": "32",
+    "key": "letsalwaysrembertohavefun",
+    "title": "Let's always remember to have fun"
+  },
+  {
+    "date": "2014-11-19",
+    "id": "31",
+    "key": "sometimesiwishforthings",
+    "title": "Sometimes I Wish for Things"
+  },
+  {
+    "date": "2014-11-19",
+    "id": "30",
+    "key": "youdontdoanyofthethingsyouusedtodo",
+    "title": "You don't do any of the things you used to do"
+  },
+  {
+    "date": "2014-11-18",
+    "id": "29",
+    "key": "idontlikethissoup",
+    "title": "I don't like this soup"
+  },
+  {
+    "date": "2014-11-18",
+    "id": "28",
+    "key": "itwasalwaysyou",
+    "title": "It was always you"
+  },
+  {
+    "date": "2014-11-17",
+    "id": "27",
+    "key": "actuallyimgoingtosleeprightnow",
+    "title": "Actually, I'm going to sleep right now."
+  },
+  {
+    "date": "2014-11-17",
+    "id": "26",
+    "key": "itsalovelyday",
+    "title": "It's a lovely Day!"
+  },
+  {
+    "date": "2014-06-26",
+    "id": "25",
+    "key": "iforgotyouwererealimsogladyourestillhere",
+    "title": "I forgot you were real. I'm so glad you're still here."
+  },
+  {
+    "date": "2014-06-26",
+    "id": "24",
+    "key": "youaremyfavoriteeverything",
+    "title": "You are my favorite everything."
+  },
+  {
+    "date": "2014-06-24",
+    "id": "23",
+    "key": "icantseemtothinkveryhard",
+    "title": "I can't seem to think very hard"
+  },
+  {
+    "date": "2014-06-24",
+    "id": "22",
+    "key": "whyred",
+    "title": "Why red?"
+  },
+  {
+    "date": "2014-06-24",
+    "id": "21",
+    "key": "ifoundabug",
+    "title": "i found a bug"
+  },
+  {
+    "date": "2014-06-17",
+    "id": "20",
+    "key": "ifonlytheyhadtoldmethatinthebeginning",
+    "title": "if only they'd told me that in the beginning"
+  },
+  {
+    "date": "2014-06-17",
+    "id": "19",
+    "key": "myfavoritesunshine",
+    "title": "my favorite sunshine"
+  },
+  {
+    "date": "2014-06-17",
+    "id": "18",
+    "key": "icantbelieveistayedupallnight",
+    "title": "I can't believe I stayed up all night"
+  },
+  {
+    "date": "2014-06-03",
+    "id": "17",
+    "key": "thedaythebirdscried",
+    "title": "the day the birds cried"
+  },
+  {
+    "date": "2014-06-03",
+    "id": "16",
+    "key": "thisguitarhasamindofitsown",
+    "title": "This guitar has a mind of its own!"
+  },
+  {
+    "date": "2014-05-30",
+    "id": "15",
+    "key": "iwantsomecolor",
+    "title": "I want some color"
+  },
+  {
+    "date": "2014-05-30",
+    "id": "14",
+    "key": "shenevercamehome",
+    "title": "She never came home"
+  },
+  {
+    "date": "2014-05-14",
+    "id": "13",
+    "key": "itgetssocrowdedinhere",
+    "title": "It gets so crowded in here."
+  },
+  {
+    "date": "2014-05-14",
+    "id": "12",
+    "key": "theressomethingsidliketotellyou",
+    "title": "there's some things I'd like to tell you"
+  },
+  {
+    "date": "2014-05-14",
+    "id": "11",
+    "key": "iwishyouneverknewaboutmysecret",
+    "title": "I wish you never knew about my secret"
+  },
+  {
+    "date": "2014-02-25",
+    "id": "10",
+    "key": "iwasntready",
+    "title": "I wasn't ready"
+  },
+  {
+    "date": "2014-02-25",
+    "id": "9",
+    "key": "allthethings",
+    "title": "All the things"
+  },
+  {
+    "date": "2014-02-14",
+    "id": "8",
+    "key": "smiletointeract",
+    "title": "Smile to Interact !"
+  },
+  {
+    "date": "2014-02-14",
+    "id": "7",
+    "key": "youarenowfreetomoveabout",
+    "title": "You are now free to move about."
+  },
+  {
+    "date": "2014-02-06",
+    "id": "6",
+    "key": "everythingishappeningididit",
+    "title": "Everything is happening. I did it."
+  },
+  {
+    "date": "2014-02-06",
+    "id": "5",
+    "key": "yourbarewallsaredepressingtome",
+    "title": "Your bare walls are depressing to me. "
+  },
+  {
+    "date": "2014-01-30",
+    "id": "4",
+    "key": "idontknowwhyithurtsme",
+    "title": "I don't know why it hurts me"
+  },
+  {
+    "date": "2014-01-30",
+    "id": "3",
+    "key": "iwanttoengineersomethingcool",
+    "title": "I Want to Engineer Something Cool"
+  },
+  {
+    "date": "2014-01-20",
+    "id": "2",
+    "key": "thanksfortheadviceithink",
+    "title": "Thanks for the advice, I think."
+  },
+  {
+    "date": "2014-01-20",
+    "id": "1",
+    "key": "imheadingnorthagain",
+    "title": "I'm heading north again."
+  },
+]
