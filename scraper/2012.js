fba88be9fec30e2a0d736d10d08236758bb320ec
Patrick Fisher <patrick@pwfisher.com>
2020-12-01 15:59:19 -0800

    update 2012

--- scraper/2012.js
+++ scraper/2012.js
@@ -1,231 +1,277 @@
 module.exports = [
   {
     "date": "2012-12-18",
+    "id": "46",
     "key": "itwasweird",
-    "title": "It was weird."
+    "title": "It was weird"
   },
   {
     "date": "2012-12-18",
+    "id": "45",
     "key": "itseasierwhenimalone",
     "title": "It's easier when I am alone"
   },
   {
     "date": "2012-11-20",
+    "id": "44",
     "key": "weweresupposedtobethereonfriday",
     "title": "We were supposed to be there on friday"
   },
   {
     "date": "2012-11-20",
+    "id": "43",
     "key": "thereisonlyone",
     "title": "There is only one"
   },
   {
     "date": "2012-10-19",
+    "id": "42",
     "key": "iwillbehappywhenitsallover",
     "title": "I will be happy when it's all over"
   },
   {
     "date": "2012-10-19",
+    "id": "41",
     "key": "depressionrobotwantstomalfunction",
     "title": "depression robot wants to malfunction"
   },
   {
     "date": "2012-10-12",
+    "id": "40",
     "key": "thesunismakingmetired",
     "title": "The Sun is making me tired"
   },
   {
     "date": "2012-10-12",
+    "id": "39",
     "key": "whyareyousogrumpywhenwehavescience",
     "title": "Why are you so grumpy when we have science?"
   },
   {
     "date": "2012-09-21",
+    "id": "38",
     "key": "themoonisbig",
     "title": "The Moon is Big"
   },
   {
     "date": "2012-09-21",
+    "id": "37",
     "key": "imtryingtonotforget",
-    "title": "I'm trying to remember to not forget..."
+    "title": "I'm trying to not forget..."
   },
   {
     "date": "2012-09-17",
+    "id": "36",
     "key": "imontoyou",
     "title": "I'm on to you"
   },
   {
     "date": "2012-09-17",
+    "id": "35",
     "key": "YOOOOUCAAAANDOOOOOOIIIIIIIIIT",
     "title": "YOOOOU CAAAAN DOOOOOO IIIIIIIIIT!"
   },
   {
     "date": "2012-09-06",
+    "id": "34",
     "key": "icanflybutonlyforasecond",
     "title": "I can fly, but only for a second"
   },
   {
     "date": "2012-09-06",
+    "id": "33",
     "key": "heliestokeepmefromgettingmad",
     "title": "He lies to keep me from getting mad"
   },
   {
     "date": "2012-08-30",
+    "id": "32",
     "key": "ikeepmydreamsunderthebed",
     "title": "I keep my Dreams under the bed"
   },
   {
     "date": "2012-08-30",
+    "id": "31",
     "key": "inthiscitytheressomanypeoplethatarentyou",
     "title": "In this city, there's so many people that aren't you."
   },
   {
     "date": "2012-08-29",
+    "id": "30",
     "key": "ifeelvictorious",
     "title": "I feel victorious"
   },
   {
     "date": "2012-08-29",
+    "id": "29",
     "key": "outheretheresnosuchthingasano",
     "title": "Out here there is no such thing as a no"
   },
   {
     "date": "2012-07-30",
+    "id": "28",
     "key": "precioussecondsofyourlife",
     "title": "Precious seconds of your life"
   },
   {
     "date": "2012-07-30",
+    "id": "27",
     "key": "lifeisbetterwithadog",
     "title": "Life is better with a dog"
   },
   {
     "date": "2012-07-25",
+    "id": "26",
     "key": "forestthinking",
-    "title": "Forest thinking"
+    "title": "Forest Thinking"
   },
   {
     "date": "2012-07-25",
+    "id": "25",
     "key": "theworldlooksbetterupsidedown",
     "title": "The world looks better upside-down."
   },
   {
     "date": "2012-07-20",
+    "id": "24",
     "key": "icanmaketherainstop",
     "title": "I can make the rain stop"
   },
   {
     "date": "2012-07-20",
+    "id": "23",
     "key": "youarenotabird",
     "title": "You're not a bird"
   },
   {
     "date": "2012-06-14",
+    "id": "22",
     "key": "whatsinstoreforustoday",
     "title": "What's in store for us today?"
   },
   {
     "date": "2012-06-14",
+    "id": "21",
     "key": "dontforgettofly",
     "title": "Don't forget to fly"
   },
   {
     "date": "2012-05-31",
+    "id": "20",
     "key": "momentslikethisneverlast",
     "title": "Moments like this never last"
   },
   {
     "date": "2012-05-31",
+    "id": "19",
     "key": "imustbedoingsomethingwrong",
     "title": "I must be doing something wrong"
   },
   {
     "date": "2012-05-31",
+    "id": "18",
     "key": "thisisnttherightwayturnback",
     "title": "This isn't the right way. Turn back."
   },
   {
     "date": "2012-04-26",
+    "id": "17",
     "key": "iamnotthesamewithoutyou",
     "title": "I am not the same without you"
   },
   {
     "date": "2012-04-26",
+    "id": "16",
     "key": "ithoughtwewouldalwaysbehere",
     "title": "I thought we would always be here"
   },
   {
     "date": "2012-04-11",
+    "id": "15",
     "key": "dogswithinsomnia",
     "title": "Dogs with insomnia"
   },
   {
     "date": "2012-04-11",
+    "id": "14",
     "key": "thejourneyisabouttoend",
     "title": "The journey is about to end"
   },
   {
     "date": "2012-04-04",
+    "id": "13",
     "key": "inoticed",
     "title": "I noticed"
   },
   {
     "date": "2012-04-04",
+    "id": "12",
     "key": "imisstheoldyou",
     "title": "I miss the old you"
   },
   {
     "date": "2012-03-21",
+    "id": "11",
     "key": "thedaygoessomuchslowerwithoutyouhere",
     "title": "The day goes much slower without you here"
   },
   {
     "date": "2012-03-21",
+    "id": "10",
     "key": "ifonlythismadesense",
     "title": "If only this made sense"
   },
   {
     "date": "2012-03-14",
+    "id": "9",
     "key": "imissthesunshine",
     "title": "I miss the sunshine"
   },
   {
     "date": "2012-03-14",
+    "id": "8",
     "key": "itsalwaysthelastplaceyoulook",
     "title": "It's always the last place you look."
   },
   {
     "date": "2012-03-13",
+    "id": "7",
     "key": "therobotwhichiliveinhasunfortunatelymalfunctioned",
     "title": "The robot which I live in has unfortunately malfunctioned..."
   },
   {
     "date": "2012-03-13",
+    "id": "6",
     "key": "notoneofmyfinestideasimustsay",
     "title": "Not one of my finest ideas I must say"
   },
   {
     "date": "2012-02-07",
+    "id": "5",
     "key": "atleastyourehappiernow",
     "title": "At least you're happier now"
   },
   {
     "date": "2012-02-07",
+    "id": "4",
     "key": "youandthatbird",
     "title": "You and that bird"
   },
   {
     "date": "2012-02-07",
+    "id": "3",
     "key": "checkoutmyspaceship",
     "title": "Check out my awesome spaceship! It can't fly but at least it can swim."
   },
   {
     "date": "2012-01-24",
+    "id": "2",
     "key": "justbecause",
     "title": "Just Because"
   },
   {
     "date": "2012-01-24",
+    "id": "1",
     "key": "whatadumbsummer",
     "title": "What a dumb summer"
   }
