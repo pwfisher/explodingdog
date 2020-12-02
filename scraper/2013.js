89459fa8c6b531cb9f2f3a2b276577b9576a3adb
Patrick Fisher <patrick@pwfisher.com>
2020-12-01 16:01:04 -0800

    update 2013

--- scraper/2013.js
+++ scraper/2013.js
@@ -1,216 +1,247 @@
 module.exports = [
   {
     "date": "2013-12-06",
+    "id": "43",
     "key": "untitled",
     "title": "Untitled"
   },
   {
-    "date": "2013-12-06",
-    "key": "doyoulikefood",
-    "title": "Do You Like Food?"
-  },
-  {
     "date": "2013-11-20",
+    "id": "41",
     "key": "youmakemedisappear",
     "title": "You make me disappear"
   },
   {
     "date": "2013-11-20",
+    "id": "40",
     "key": "twopenguinsallalone",
     "title": "Two penguins, all alone"
   },
   {
     "date": "2013-11-07",
+    "id": "39",
     "key": "wheredowego2013",
     "title": "Where do we go?"
   },
   {
     "date": "2013-11-07",
+    "id": "38",
     "key": "looktothesky2013",
     "title": "Looking to the sky"
   },
   {
     "date": "2013-10-18",
+    "id": "37",
     "key": "nothing-justeverything",
     "title": "nothing...just everything"
   },
   {
     "date": "2013-10-18",
+    "id": "36",
     "key": "thisisntrealsnow",
     "title": "this isn't real snow"
   },
   {
     "date": "2013-10-08",
+    "id": "35",
     "key": "notquitethere",
-    "title": "Not quite there"
+    "title": "not quite there"
   },
   {
     "date": "2013-10-08",
+    "id": "34",
     "key": "theworldisquiethere",
-    "title": "The world is quiet here."
+    "title": "The world is quiet here"
   },
   {
     "date": "2013-10-03",
+    "id": "33",
     "key": "ihopeillmakeit",
     "title": "I hope I'll make it"
   },
   {
     "date": "2013-10-03",
+    "id": "32",
     "key": "thisconversationisover",
     "title": "This conversation is over."
   },
   {
     "date": "2013-09-30",
+    "id": "31",
     "key": "shhimtryintobecreative",
     "title": "shh, I am trying to be creative."
   },
   {
     "date": "2013-09-30",
+    "id": "30",
     "key": "ithoughtiwouldfindyoubynow",
     "title": "I thought I'd find you by now"
   },
   {
     "date": "2013-09-30",
+    "id": "29",
     "key": "everyonceinawhileiremeberwhatitisliketofly",
     "title": "Every once in a while I remember what it was like to fly"
   },
   {
     "date": "2013-09-26",
+    "id": "28",
     "key": "iwishicouldseeyouagain",
     "title": "I wish I could see you again"
   },
   {
     "date": "2013-09-26",
-    "key": "iwishthaticouldseeyousoon",
-    "title": "I wish that I could see you soon."
-  },
-  {
-    "date": "2013-09-26",
+    "id": "26",
     "key": "itwillneverbetodayagain",
     "title": "It will never be today again"
   },
   {
     "date": "2013-09-19",
+    "id": "25",
     "key": "comebacktomethedragonissleeping",
     "title": "Come back to me, the dragon is sleeping"
   },
   {
     "date": "2013-09-19",
+    "id": "24",
     "key": "howcanigetthrough",
     "title": "How can I get through?"
   },
   {
     "date": "2013-09-12",
+    "id": "23",
     "key": "ihaventquitefigureditoutyet",
     "title": "I haven't quite figured it out yet"
   },
   {
     "date": "2013-09-12",
+    "id": "22",
     "key": "youcantstopmenow",
     "title": "You can't stop me now."
   },
   {
     "date": "2013-08-22",
+    "id": "21",
     "key": "wellitookyouradvice",
     "title": "Well, I took your advice"
   },
   {
     "date": "2013-08-22",
+    "id": "20",
     "key": "itsalwaystoolate-2",
     "title": "It's always too late"
   },
   {
     "date": "2013-08-08",
+    "id": "19",
     "key": "ijustcantlivelikethisanymore",
     "title": "I just can't live like this anymore"
   },
   {
     "date": "2013-08-08",
+    "id": "18",
     "key": "theresdogseverywhere",
     "title": "There's dogs everywhere!"
   },
   {
     "date": "2013-08-01",
+    "id": "17",
     "key": "everythingwentnowhereforever",
     "title": "Everything went nowhere forever."
   },
   {
     "date": "2013-08-01",
+    "id": "16",
     "key": "thisisnottherobotiaskedfor",
     "title": "This is not the robot I asked for"
   },
   {
     "date": "2013-07-24",
+    "id": "15",
     "key": "everyonehereisweirdedthanweare",
     "title": "Everyone here is weirder than we are"
   },
   {
     "date": "2013-07-24",
+    "id": "14",
     "key": "weareluckywhenwestayupallnight",
     "title": "WE ARE LUCKY WHEN WE STAY UP ALL NIGHT"
   },
   {
     "date": "2013-05-31",
+    "id": "13",
     "key": "ifallasleeptothesoundofyourmachine",
     "title": "I fall asleep to the sound of your machine."
   },
   {
     "date": "2013-05-31",
+    "id": "12",
     "key": "imakethesamemistakes",
     "title": "I make the same mistakes"
   },
   {
     "date": "2013-05-28",
+    "id": "11",
     "key": "oneofusshouldntbehere",
     "title": "One of us shouldn't be here"
   },
   {
     "date": "2013-05-28",
+    "id": "10",
     "key": "intwohoursok",
     "title": "In two hours, ok?"
   },
   {
     "date": "2013-04-17",
+    "id": "9",
     "key": "wellneedyoutobuildacastle",
     "title": "we'll need you to build a castle."
   },
   {
     "date": "2013-04-17",
+    "id": "8",
     "key": "imgoingtobuyapowerdrill",
     "title": "I'm going to buy a power drill."
   },
   {
     "date": "2013-04-17",
+    "id": "7",
     "key": "whydidithinkthatwasagoodidea",
     "title": "why did I think that was a good idea?!"
   },
   {
     "date": "2013-04-05",
+    "id": "6",
     "key": "maybeweweremeanttobe",
     "title": "Maybe we were meant to be"
   },
   {
     "date": "2013-04-05",
+    "id": "5",
     "key": "sometimesijustdontknowwhentoquit",
     "title": "sometimes I just don't know when to quit..."
   },
   {
     "date": "2013-03-29",
+    "id": "4",
     "key": "collectingrelicsofasimplertime",
     "title": "Collecting relics of a simpler time."
   },
   {
     "date": "2013-03-29",
+    "id": "3",
     "key": "theresatickinginmyhead",
     "title": "There's a ticking in my head"
   },
   {
     "date": "2013-03-20",
+    "id": "2",
     "key": "thehardestpartisgettingstarted",
-    "title": "The hardest part is getting starting."
+    "title": "The hardest part is getting started"
   },
   {
     "date": "2013-03-20",
+    "id": "1",
     "key": "itwasbetterbefore",
     "title": "It was better before"
   }
