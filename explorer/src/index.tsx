c1564a6a3bef8116bcba63fde52b14cd19d1db9c
Patrick Fisher <patrick@pwfisher.com>
2020-12-02 14:26:16 -0800

    create /lib, /components

--- explorer/src/index.tsx
+++ explorer/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { App } from './components/App';
+import { reportWebVitals } from './lib/reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
