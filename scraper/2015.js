aac22e2826cce8e4c836a3b5616d03e21beb6ddd
Patrick Fisher <patrick@pwfisher.com>
2020-12-01 16:03:49 -0800

    fix 2015 data

--- scraper/2015.js
+++ scraper/2015.js
@@ -1,302 +1,302 @@
 module.exports = [
   {
     "date": "2015-12-10",
-    "id:": "50",
+    "id": "50",
     "key": "ipromisedragonsexist",
     "title": "I promise dragons exist"
   },
   {
     "date": "2015-12-04",
-    "id:": "49",
+    "id": "49",
     "key": "youchooseyourchoices",
     "title": "you choose your choices"
   },
   {
     "date": "2015-11-13",
-    "id:": "48",
+    "id": "48",
     "key": "iliedagain",
     "title": "I lied again."
   },
   {
     "date": "2015-10-07",
-    "id:": "47",
+    "id": "47",
     "key": "ihatethismachine",
     "title": "I hate this machine"
   },
   {
     "date": "2015-10-02",
-    "id:": "46",
+    "id": "46",
     "key": "itcantmeannothingcanit",
     "title": "It can't mean nothing. Can it?"
   },
   {
     "date": "2015-09-13",
-    "id:": "45",
+    "id": "45",
     "key": "andthenyoulaughed",
     "title": "And then you laughed"
   },
   {
     "date": "2015-09-11",
-    "id:": "44",
+    "id": "44",
     "key": "ididntknowhecouldtalk",
     "title": "I didn't know he could talk"
   },
   {
     "date": "2015-09-10",
-    "id:": "43",
+    "id": "43",
     "key": "iwanttoscream",
     "title": "I want to scream"
   },
   {
     "date": "2015-09-09",
-    "id:": "42",
+    "id": "42",
     "key": "imfarbeyondthecliffsofnoreturn",
     "title": "I'm far beyond the Cliffs of No return"
   },
   {
     "date": "2015-09-04",
-    "id:": "41",
+    "id": "41",
     "key": "mytherapistknowsallaboutyou",
     "title": "my therapist knows all about you"
   },
   {
     "date": "2015-09-02",
-    "id:": "40",
+    "id": "40",
     "key": "youdontknowthehumanivebecome",
     "title": "you don't know the human I've become"
   },
   {
     "date": "2015-06-15",
-    "id:": "39",
+    "id": "39",
     "key": "ijustneedtojumpthecliff",
-    "title": "I just need to jump the cliff",
-  }
-    {
-      "date": "2015-06-15",
-      "id:": "38",
+    "title": "I just need to jump the cliff"
+  },
+  {
+    "date": "2015-06-15",
+    "id": "38",
     "key": "theawakeningofthesoul",
-    "title": "The awakening of the soul",
+    "title": "The awakening of the soul"
   },
   {
     "date": "2015-06-03",
-    "id:": "37",
+    "id": "37",
     "key": "sheisaconstantmiracle",
-    "title": "she is a constant miracle",
-  },
-{
-"date": "2015-06-03",
-"id:": "36",
-"key": "abirdandabear",
-    "title": "a bird and a bear",
+    "title": "she is a constant miracle"
   },
   {
     "date": "2015-06-03",
-    "id:": "35",
+    "id": "36",
+    "key": "abirdandabear",
+    "title": "a bird and a bear"
+  },
+  {
+    "date": "2015-06-03",
+    "id": "35",
     "key": "iwontdoitunlessyouhelpme",
     "title": "I won't do it unless you help me"
   },
   {
     "date": "2015-05-13",
-    "id:": "34",
+    "id": "34",
     "key": "dearfreckles",
-    "title": "Dear Freckles, If you want to stay you have to pay the rent.",
+    "title": "Dear Freckles, If you want to stay you have to pay the rent."
   },
   {
     "date": "2015-05-13",
-    "id:": "33",
+    "id": "33",
     "key": "iamyourelephant",
     "title": "I am your elephant"
   },
   {
     "date": "2015-05-05",
-    "id:": "32",
+    "id": "32",
     "key": "icantkeeptime",
-    "title": "I can't keep time",
+    "title": "I can't keep time"
   },
   {
     "date": "2015-05-05",
-    "id:": "31",
+    "id": "31",
     "key": "othersideoftheworld",
     "title": "other side of the world"
   },
   {
     "date": "2015-04-01",
-    "id:": "30",
+    "id": "30",
     "key": "herehavethiskeyifounditontheground",
-    "title": "Here, have this key. I found it on the ground.",
+    "title": "Here, have this key. I found it on the ground."
   },
   {
     "date": "2015-04-01",
-    "id:": "29",
+    "id": "29",
     "key": "myideaithoughtthiswasyouridea",
     "title": "My idea? I thought this was your idea."
   },
   {
     "date": "2015-03-27",
-    "id:": "28",
+    "id": "28",
     "key": "ihopeyouwerentcold",
-    "title": "I hope you weren't cold",
+    "title": "I hope you weren't cold"
   },
   {
     "date": "2015-03-27",
-    "id:": "27",
+    "id": "27",
     "key": "qualitycontrolsaysthisoneisgood",
     "title": "Quality control says this one is good"
   },
   {
     "date": "2015-03-17",
-    "id:": "26",
+    "id": "26",
     "key": "pleaseremembermewhenyougetthere",
-    "title": "Please remember me when you get there.",
+    "title": "Please remember me when you get there."
   },
   {
     "date": "2015-03-17",
-    "id:": "25",
+    "id": "25",
     "key": "itsgettinglatebutidontmind",
     "title": "It's getting late but I don't mind"
   },
   {
     "date": "2015-03-06",
-    "id:": "24",
+    "id": "24",
     "key": "iwastoldtherewouldbenomath",
-    "title": "I was told there would be no math",
+    "title": "I was told there would be no math"
   },
   {
     "date": "2015-03-06",
-    "id:": "23",
+    "id": "23",
     "key": "thisismypieceofdirt",
     "title": "This is my piece of dirt"
   },
   {
     "date": "2015-03-04",
-    "id:": "22",
+    "id": "22",
     "key": "therobotsarelearning",
-    "title": "The Robots are Learning",
+    "title": "The Robots are Learning"
   },
   {
     "date": "2015-03-04",
-    "id:": "21",
+    "id": "21",
     "key": "whatshouldisay",
     "title": "What Should I Say?"
   },
   {
     "date": "2015-03-02",
-    "id:": "20",
+    "id": "20",
     "key": "iamnotthesameasiwasindecember",
-    "title": "I am not the same as I was in December",
+    "title": "I am not the same as I was in December"
   },
   {
     "date": "2015-03-02",
-    "id:": "19",
+    "id": "19",
     "key": "togetherforever",
     "title": "Together Forever"
   },
   {
     "date": "2015-02-27",
-    "id:": "18",
+    "id": "18",
     "key": "wejustcouldntanymore",
-    "title": "we just couldn't anymore",
+    "title": "we just couldn't anymore"
   },
   {
     "date": "2015-02-27",
-    "id:": "17",
+    "id": "17",
     "key": "whatamieating",
     "title": "What am I eating?"
   },
   {
     "date": "2015-02-09",
-    "id:": "16",
+    "id": "16",
     "key": "alwaysonrepeat",
-    "title": "Always on Repeat",
+    "title": "Always on Repeat"
   },
   {
     "date": "2015-02-09",
-    "id:": "15",
+    "id": "15",
     "key": "itsthattimeofyearagain",
     "title": "It's that time of year again"
   },
   {
     "date": "2015-01-28",
-    "id:": "14",
+    "id": "14",
     "key": "youcantbeidealisticallyourlife",
-    "title": "You can't be idealistic all your life",
+    "title": "You can't be idealistic all your life"
   },
   {
     "date": "2015-01-28",
-    "id:": "13",
+    "id": "13",
     "key": "everythingsbetterthesecondtimearound",
     "title": "Everything's better the second time around"
   },
   {
     "date": "2015-01-27",
-    "id:": "12",
+    "id": "12",
     "key": "youaremyfavoritesnowflake",
-    "title": "You are my favorite snowflake",
+    "title": "You are my favorite snowflake"
   },
   {
     "date": "2015-01-27",
-    "id:": "11",
+    "id": "11",
     "key": "ithoughtwednevermeet",
     "title": "I thought we'd never meet"
   },
   {
     "date": "2015-01-15",
-    "id:": "9",
+    "id": "9",
     "key": "icantlookthatfarback",
-    "title": "I can't look that far back",
+    "title": "I can't look that far back"
   },
   {
     "date": "2015-01-15",
-    "id:": "10",
+    "id": "10",
     "key": "hesawkwardashellbutilovehim",
     "title": "He's awkward as hell, but I love him"
   },
   {
     "date": "2015-01-14",
-    "id:": "8",
+    "id": "8",
     "key": "ifeelmuchbetternow",
-    "title": "I feel much better now",
+    "title": "I feel much better now"
   },
   {
     "date": "2015-01-14",
-    "id:": "7",
+    "id": "7",
     "key": "illgobutiwontgetoutofthecar",
     "title": "I'll go but I won't get out of the car"
   },
   {
     "date": "2015-01-09",
-    "id:": "6",
+    "id": "6",
     "key": "sometimesisitinmyclosetandthinkofalltheterriblethingsivedone",
-    "title": "sometimes I sit in my closet and think of all the terrible things I've done",
+    "title": "sometimes I sit in my closet and think of all the terrible things I've done"
   },
   {
     "date": "2015-01-09",
-    "id:": "5",
+    "id": "5",
     "key": "whatdoyouthink",
     "title": "What do you think?"
   },
   {
     "date": "2015-01-08",
-    "id:": "4",
+    "id": "4",
     "key": "thankyouforsavingmylife",
-    "title": "Thank you for saving my life",
+    "title": "Thank you for saving my life"
   },
   {
     "date": "2015-01-08",
-    "id:": "3",
+    "id": "3",
     "key": "maybeyoullnevercryagain",
     "title": "Maybe you'll never cry again"
   },
   {
     "date": "2015-01-06",
-    "id:": "2",
+    "id": "2",
     "key": "leavingtheoldbehind",
-    "title": "Leaving the Old Behind",
+    "title": "Leaving the Old Behind"
   },
   {
     "date": "2015-01-06",
-    "id:": "1",
+    "id": "1",
     "key": "myresolutionistoquiteating",
     "title": "My resolution is to quit eating"
-  },
-]
+  }
+];
