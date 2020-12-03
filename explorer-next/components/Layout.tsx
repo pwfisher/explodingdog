3dfa8efa354119ff516feaea8c14774b5d988643
Patrick Fisher <patrick@pwfisher.com>
2020-12-02 16:19:46 -0800

    repeat changes to explorer-next (icon, gitignore, package.json)

--- explorer-next/components/Layout.tsx
+++ explorer-next/components/Layout.tsx
@@ -5,29 +5,40 @@ import Head from 'next/head'
 type Props = {
   children?: ReactNode
   title?: string
+  description?: string
 }
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
+const Layout = ({
+  children,
+  title = 'explodingdog explorer',
+  description = 'explodingdog explorer. An alternate explodingdog experience.'
+}: Props) => (
   <div>
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      <meta name="theme-color" content="#000000" />
+      <meta name="description" content={description} />
+      <link rel="manifest" href="/manifest.json" />
     </Head>
     <header>
       <nav>
         <Link href="/">
           <a>Home</a>
-        </Link>{' '}
-        |{' '}
+        </Link>
+        {' | '}
         <Link href="/about">
           <a>About</a>
-        </Link>{' '}
-        |{' '}
+        </Link>
+        {' | '}
         <Link href="/users">
           <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
+        </Link>
+        {' | '}
+        <a href="/api/users">Users API</a>
       </nav>
     </header>
     {children}
