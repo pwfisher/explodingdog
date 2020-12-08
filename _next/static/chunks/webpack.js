c9fc948da6a100936a24b91889d90778b0a6211b
Patrick Fisher <patrick@pwfisher.com>
2020-12-07 17:13:25 -0800

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a27417f15a172c81449f";
+/******/ 	var hotCurrentHash = "2c224639121aab7036d1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
